823287beb0df053127cd350f15fbaadece8c5106
Shauyenov_A <akzhan133@gmail.com>
2025-01-15 16:41:49 +0500

    Commit after build

--- src/http/userApi.js
+++ src/http/userApi.js
@@ -3,7 +3,7 @@ import {jwtDecode} from 'jwt-decode';
 
 export const registration = async (email, password) => {
     try {
-        const { data } = await $host.post('api/user/registration', { email, password, role: 'ADMIN' });
+        const { data } = await $host.post('/api/user/registration', { email, password, role: 'ADMIN' });
         localStorage.setItem('token', data.token);
         return jwtDecode(data.token);
     } catch (error) {
@@ -14,7 +14,7 @@ export const registration = async (email, password) => {
 
 export const login = async (email, password) => {
     try {
-        const { data } = await $host.post('api/user/login', { email, password });
+        const { data } = await $host.post('/api/user/login', { email, password });
         localStorage.setItem('token', data.token);
         return jwtDecode(data.token);
     } catch (error) {
@@ -25,7 +25,7 @@ export const login = async (email, password) => {
 
 export const check = async () => {
     try {
-        const { data } = await $authHost.get('api/user/auth');
+        const { data } = await $authHost.get('/api/user/auth');
         localStorage.setItem('token', data.token);
         return jwtDecode(data.token); // Decode the token to extract user data
     } catch (error) {
